382fa2ba7c3d4171b9184ed032ee283e6262f8b5
Muatasim Qazi <muatasimqazi@gmail.com>
2017-10-11 20:48:11 -0700

    added signup route

--- booking_app.py
+++ booking_app.py
@@ -1,15 +1,18 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 
 app = Flask(__name__)
-
 app.config['DEBUG'] = True
-
 
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html', ), 404
 
+@app.route('/signup', methods=['POST', 'GET'])
+def signup():
+    return render_template('signup.html')
+
 @app.route("/")
+@app.route('/home')
 def index():
     return render_template('base.html')
 
