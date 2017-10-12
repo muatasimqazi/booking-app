bdbbe34689dbb561535286c93d4a161082fe1b8c
Muatasim Qazi <muatasimqazi@gmail.com>
2017-10-11 20:03:01 -0700

    project setup

--- booking_app.py
+++ booking_app.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for, session, flash
 
 app = Flask(__name__)
+
 app.config['DEBUG'] = True
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html', ), 404
 
 @app.route("/")
 def index():
-    return "Hello World"
+    return render_template('base.html')
 
 app.run()
