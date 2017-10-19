f12b8ddc21b5790f0740ddd0fa2ba0f789aa3b69
Abdulkerim <abdulkerim333@gmail.com>
2017-10-18 19:44:48 -0700

    aded event on quickstart.py

--- quickstart.py
+++ quickstart.py
@@ -72,7 +72,7 @@ def main():
     # stored credentials.
 
     event = {
-      'summary': 'Google I/O 2015',
+      'summary': 'Booking app',
       'location': '800 Howard St., San Francisco, CA 94103',
       'description': 'A chance to hear more about Google\'s developer products.',
       'start': {
